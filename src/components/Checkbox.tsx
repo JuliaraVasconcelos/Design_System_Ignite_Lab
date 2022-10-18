88b34b4c61aa952db31d3e8ef23208d2efc8687d
Juliara Vasconcelos <40279322+JuliaraVasconcelos@users.noreply.github.com>
2022-10-18 12:00:57 -0300

    login page using storybook compnents

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,15 +2,12 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 import { ReactNode } from "react";
 
-export interface CheckboxProps {
-    children: ReactNode;
-    asChild: boolean;
-}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export function Checkbox({ }: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
     return (
         <CheckboxPrimitive.Root
-            className='w-6 h-6 p-1 bg-gray-800 rounded'>
+            className='w-6 h-6 p-1 bg-gray-800 rounded' {...props}>
             <CheckboxPrimitive.Indicator asChild>
                 <Check weight='bold' className='h-5 w-5 text-cyan-500'/>
             </CheckboxPrimitive.Indicator>
